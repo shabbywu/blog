79039bcfc50e99425edbc52f3e5e7677f11ceafb
Wu jingtao <shabbywu@qq.com>
2024-10-14 21:38:03 +0800

    update resume

--- src/.components/resume/Resume.zh-cn.vue
+++ src/.components/resume/Resume.zh-cn.vue
@@ -74,28 +74,80 @@
             <section class="section projects-section">
                 <h2 class="section-title"><span class="icon-holder"><i class="fa-solid fa-archive"></i></span>项目</h2>
                 <div class="item">
-                    <span class="project-title"><a href="https://github.com/TencentBlueKing/blueking-paas" target="_blank">Blueking PaaS</a></span> - <span class="project-tagline">蓝鲸智云 PaaS 平台是一个开放式的开发平台，在腾讯内部托管运行着 1600+ SaaS 应用。该项目的 Web API 使用 Django(DRF) 和 Celery 开发, 应用构建依托于 Cloud Native Buildpacks 和 Kaniko, 应用运行生命周期使用 K8S Operator(Golang) 管理。接入层使用 Nginx Ingress 作流量转发, 支持常规 ELK 方案和蓝鲸自研日志采集器采集日志。</span>
-                    <blockquote class="project-roleline">我在该项目的角色是后台开发，基本上所有的核心功能如应用构建与部署、应用离线交付方案、接入层管理、日志采集与查询等都由我主导开发。</blockquote>
+                    <div class="project-title">
+                        <span class="darken">名称: </span>
+                        <div class="detail">
+                            <a href="https://github.com/TencentBlueKing/blueking-paas" target="_blank">Blueking PaaS</a>
+                            <span class="timetag">2019-2024</span>
+                        </div>
+                    </div>
+                    <div class="project-description">
+                        <span class="darken">描述: </span>
+                        <div class="detail">蓝鲸智云 PaaS 平台是一个开放式的开发平台，在腾讯内部托管运行着 1600+ SaaS 应用。该项目的 Web API 使用 Django(DRF) 和 Celery 开发, 应用构建依托于 Cloud Native Buildpacks 和 Kaniko, 应用运行生命周期使用 K8S Operator(Golang) 管理。接入层使用 Nginx Ingress 作流量转发, 支持常规 ELK 方案和蓝鲸自研日志采集器采集日志。</div>
+                    </div>
+                    <div class="project-roleblock">
+                        <div class="darken">职责: </div>
+                        <div class="detail">
+                            <!-- <div class="project-roleline">我在该项目的角色是后台开发，基本上所有的核心功能如应用构建与部署、应用离线交付方案、接入层管理、日志采集与查询等都由我主导开发。</div> -->
+                            <div class="project-roleline">※ 设计从源码到构建的执行流程和数据存储结构, 实现从 heroku-buildpack 到 cloud native buildpack 架构的无缝升级。</div>
+                            <div class="project-roleline">※ 设计应用(SaaS)离线交付方案和构建工具, 支持以镜像层为粒度交付应用(SaaS)的镜像。通过自研的镜像层合并方案降低容器镜像的交付体积。</div>
+                            <div class="project-roleline">※ 运用设计原则和对模板、组合、策略等设计模式的综合使用, 搭建了同时支持 ELK 和蓝鲸日志平台的日志采集、查询链路；搭建了支持 git/svn/容器镜像的应用源码(或镜像)的源码拉取模块。</div>
+                            <div  class="project-roleline">※ 设计了一套用于开发后台管理页面的开发流程，不仅可以用 Vue 还能使用 Django 的 Server Side Rendering, 降低了 80% 后台管理页面的接口开发复杂度。</div>
+                        </div>
+                    </div>
                 </div><!--//item-->
 
                 <div class="item">
-                    <span class="project-title"><a href="javascript:void(0);">Career Homepage</a></span> - <span class="project-tagline">Career Homepage 是腾讯 IEG 技术运营部内的职员画像系统。该系统以蓝鲸运维基础数据平台为基座, 使用可配置的指标计算系统从海量自动化、运维开发、DevOps、AIOps四个维度量化职员能力, 每个绩效评估周期都会用于评估技术营业部 500+ 职员能力。</span>
-                    <blockquote class="project-roleline">我在该项目的角色是后台开发，主要负责与产品讨论和分析需求，设计实现方案、完成代码开发以及与前端联调。</blockquote>
-                </div><!--//item-->
+                    <div class="project-title">
+                        <span class="darken">名称: </span>
+                        <div class="detail">
+                            <a href="javascript:void(0);">Career Homepage</a>
+                            <span class="timetag">2020-2024</span>
+                        </div>
+                    </div>
 
-                <div class="item">
-                    <span class="project-title"><a href="javascript:void(0);">PaaS-Analysis</a></span> - <span class="project-tagline">PaaS-Analysis 是一个轻量级的访问统计项目(Page View), 提供 js-sdk 和接入层统计两种方式计算网站的访问量。该项目使用 Fastapi、Redis 和 Lua(OpenResty) 开发。</span>
-                    <blockquote class="project-roleline">我在该项目的角色是后台开发, 主要负责开发 Web API(Fastapi)和后台任务(Python asyncio, Redis)。</blockquote>
-                </div><!--//item-->
+                    <div class="project-description">
+                        <span class="darken">描述: </span>
+                        <div class="detail">Career Homepage 是腾讯 IEG 技术运营部内的职员画像系统。该系统以蓝鲸运维基础数据平台为基座, 使用可配置的指标计算系统从海量自动化、运维开发、DevOps、AIOps四个维度量化职员能力, 每个绩效评估周期都会用于评估技术营业部 500+ 职员能力。</div>
+                    </div>
 
+                    <div class="project-roleblock">
+                        <div class="darken">职责: </div>
+                        <div class="detail">
+                            <!-- <div class="project-roleline">我在该项目的角色是后台开发，主要负责与产品讨论和分析需求，设计实现方案、完成代码开发以及与前端联调。</div> -->
+                            <div class="project-roleline">※ 设计一套可配置、易扩展、支持组合计算的指标计算与查询模块。此方案经历数次考核方案大变动，仍可满足最终的业务场景。</div>
+                            <div class="project-roleline">※ 结合腾讯晋级答辩方式和周期的调整，应业务部门的诉求从半年计算一次画像优化至使用 Celery 定时任务按天计算用户该周期内的画像评分。</div>
+                        </div>
+                    </div>
+                </div><!--//item-->
                 <hr/>
 
                 <div class="item">
-                    <span class="project-title"><a href="https://github.com/shabbywu/distribution" target="_blank">moby-distribution</a></span> - <span class="project-tagline">moby-distribution 是一个使用 python 开发的, 用于操作 Docker Image Manifest 和 Blob（Layers、Config 等）的库。这个工具库由我独立开发和维护，使用该库可实现不依赖 Docker Daemon 的镜像创建，推送等操作。目前该库被应用在 Blueking PaaS 项目, 用于合成并上传 S-Mart 应用镜像。</span>
+                    <div class="project-title">
+                        <span class="darken">名称: </span>
+                        <div class="detail">
+                            <a href="https://github.com/shabbywu/distribution" target="_blank">moby-distribution</a>
+                            <span class="timetag">2023</span>
+                        </div>
+                    </div>
+                    <div class="project-description">
+                        <span class="darken">描述: </span>
+                        <div class="detail">moby-distribution 是一个使用 python 开发的, 用于操作 Docker Image Manifest 和 Blob（Layers、Config 等）的库。这个工具库由我独立开发和维护，使用该库可实现不依赖 Docker Daemon 的镜像创建，推送等操作。目前该库被应用在 Blueking PaaS 项目, 用于合成并上传 S-Mart 应用镜像。</div>
+                    </div>
                 </div><!--//item-->
 
                 <div class="item">
-                    <span class="project-title"><a href="https://github.com/shabbywu/Battle-Brothers-CN" target="_blank">Battle-Brothers-CN</a></span> - <span class="project-tagline">Battle-Brothers-CN 是游戏 <a href="https://store.steampowered.com/app/365360/Battle_Brothers/">Battle Brothers</a> 的中文语言本地化项目。中文语言本地化项目主要分为 4 个模块 - 文本提取、文本翻译、文本替换、内核汉化器。目前该项目的日常运维(持续性的文本更新)完全基于自动化的 CI/CD 工具。</span>
+                    <div class="project-title">
+                        <span class="darken">名称: </span>
+                        <div class="detail">
+                            <a href="https://github.com/shabbywu/Battle-Brothers-CN" target="_blank">Battle-Brothers-CN</a>
+                            <span class="timetag">2023-2024</span>
+                        </div>
+                    </div>
+                    <div class="project-description">
+                        <span class="darken">描述: </span>
+                        <div class="detail">Battle-Brothers-CN 是游戏 <a href="https://store.steampowered.com/app/365360/Battle_Brothers/">Battle Brothers</a> 的中文语言本地化项目。中文语言本地化项目主要分为 4 个模块 - 文本提取、文本翻译、文本替换、内核汉化器。目前该项目的日常运维(持续性的文本更新)完全基于自动化的 CI/CD 工具。</div>
+                    </div>
                 </div><!--//item-->
             </section><!--//section-->
             
